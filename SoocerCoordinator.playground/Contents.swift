25ad9e85bcc223dfb3d2fc7cb72193a0c00fb7a9
Anthony Omenya <anthony@insidde.com>
2016-05-29 15:46:50 -0400

    Players data

--- SoocerCoordinator.playground/Contents.swift
+++ SoocerCoordinator.playground/Contents.swift
@@ -2,6 +2,45 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Players data
 
-//Tests
+let joeSmith = ["name": "Joe Smith", "height": 42, "experince": true ,"guardian": "Jim and Jan Smith"]
+
+let jillTanner = ["name":"Jill Tanner","heigth": 36, "experience": true, "gardian": "Clara Tanner"]
+
+let billBon = ["name": "Bill Bon","heigth": 43, "experience":true, "gardian": "Sara and Jenny Bon"]
+
+let evaGordon = ["name":"Eva Gordon","height": 45,"experience": false, "gardian": "Wendy and Mike Gordon"]
+
+let mattGill = ["name":"Matt Gill", "height": 40, "experience": false, "gardian": "Charles and Sylvia Gill"]
+
+let kimmStein = ["name":"Kimmy Stein","height": 41, "experience": false,"gardian": "Bill and Hillary Stein"]
+
+let sammyAdams = ["name":"Sammy Adams", "height": 45, "experience": false, "gardian": "Jeff Adams"]
+
+let karlSaygan = ["name": "Karl Saygan", "height": 42, "experience": true ,"gardian":"Heather Bledsoe"]
+
+let suzaneGreenberg = ["name":"Suzane Greenberg", "height": 44, "experience": true ,"gardian": "Henrietta Dumas"]
+
+let salDali = ["name": "Sal Dali", "height": 41, "experience": false, "gardian": "Gala Dali"]
+
+let joeKavalier = ["name": "Joe Kavalier", "heigth": 39, "experience": false, "gardian": "Sam and Elaine Kavalier"]
+
+let benFinkelstein = ["name": "Ben Finkelstein", "heigth": 44, "experience": false, "gardian": "Aaron and Jill Finkelstein"]
+
+let diegoSoto = ["name": "Diego Soto", "height": 41, "experience": true, "gardian": "Robin and Sarika Soto"]
+
+let choleAlaska = ["name": "Chloe Alaska", "height": 47, "experience": false, "gardian": "David and Jamie Alaska"]
+
+let arnoldWillis = ["name": "Arnold Willis" , "height": 43, "experience": false, "gardian": "Claire Willis"]
+
+let phillipHelm = ["name": "Phillip Helm", "height": 43, "experience": true, "gardian": "Thomas Helm and Eva Jones"]
+
+let lesClay = ["name": "Les Clay", "height": 42, "experience": true, "gradian": "Wynonna Brown"]
+
+let herschelKrustofski = ["name": "Herschel Krustofski", "height": 45, "gardia": "Hyman and Rachel Krustofski"]
+
+
+
+
+
